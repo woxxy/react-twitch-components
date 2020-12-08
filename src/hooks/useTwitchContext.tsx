efc3ab86ad52e6ae537b020782fa70f47d0aebe3
Nicolas Stepien <567105+nstepien@users.noreply.github.com>
2020-12-08 16:36:18 +0000

    Replace type assertions in useTwitchContext and useTwitchCurrentUser (#4)

--- src/hooks/useTwitchContext.tsx
+++ src/hooks/useTwitchContext.tsx
@@ -2,5 +2,12 @@ import { useContext } from 'react';
 import { TwitchAPIContext } from '../api/context';
 import { TwitchAPIContextProps } from '../api/interfaces';
 
-export const useTwitchContext = (): TwitchAPIContextProps =>
-  useContext(TwitchAPIContext) as TwitchAPIContextProps;
+export const useTwitchContext = (): TwitchAPIContextProps => {
+  const context = useContext(TwitchAPIContext);
+
+  if (context === undefined) {
+    throw new Error('useTwitchContext must be used within a TwitchProvider')
+  }
+
+  return context;
+}
--- src/hooks/useTwitchCurrentUser.tsx
+++ src/hooks/useTwitchCurrentUser.tsx
@@ -2,5 +2,12 @@ import { useContext } from 'react';
 import { TwitchCurrentUserContext } from '../currentUser/context';
 import { TwitchCurrentUserProps } from '../currentUser/interfaces';
 
-export const useTwitchCurrentUser = (): TwitchCurrentUserProps =>
-  useContext(TwitchCurrentUserContext) as TwitchCurrentUserProps;
+export const useTwitchCurrentUser = (): TwitchCurrentUserProps => {
+  const context = useContext(TwitchCurrentUserContext);
+
+  if (context === undefined) {
+    throw new Error('useTwitchCurrentUser must be used within a TwitchProvider')
+  }
+
+  return context;
+}
