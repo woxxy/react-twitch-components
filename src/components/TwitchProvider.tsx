9b0ebd8578572aedf4392b922bc22d838025f63d
Nicolas Stepien <stepien.nicolas@gmail.com>
2020-12-08 23:34:24 +0000

    rm useTwitchUsers from TwitchProvider

--- src/components/TwitchProvider.tsx
+++ src/components/TwitchProvider.tsx
@@ -1,5 +1,4 @@
 import { redirectForToken } from '../api/helpers';
-import { useTwitchUsers } from '../hooks/twitch';
 import { TwitchContext } from './TwitchContext';
 
 const clientId = '9031tpad4quocef3dgtu0bbnbupmdr';
@@ -7,14 +6,11 @@ const clientId = '9031tpad4quocef3dgtu0bbnbupmdr';
 export const TwitchProvider = ({ children }: { children: React.ReactNode }) => {
   const hashParams = new URLSearchParams(document.location.hash.substr(1));
   const accessToken = hashParams.get('access_token');
-  const { data } = useTwitchUsers();
 
   if (!accessToken) {
     redirectForToken();
     return null;
   }
-
-  if (data == null) return null;
 
   return (
     <TwitchContext.Provider
