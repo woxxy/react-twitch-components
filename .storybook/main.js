77a323ced63d201e0f3f5bce3424d2e4fd6d6d15
woxxy <woxxap@gmail.com>
2020-12-09 01:36:04 +0000

    Reformat main from storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,4 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/add on-links', '@storybook/addon-essentials'],
+};
