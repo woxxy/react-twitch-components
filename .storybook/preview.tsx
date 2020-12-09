5b016ee6118a5bccfb1e3a84dba85eaa5732eca6
woxxy <woxxap@gmail.com>
2020-12-09 18:00:25 +0000

    Make preview file from storybook work with typescript

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,11 +1,12 @@
 import { TwitchContext } from '../src/components/TwitchContext';
+import { Story } from '@storybook/react/types-6-0';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
 
 export const decorators = [
-  Story => (
+  (Story: Story) => (
     <TwitchContext.Provider
       value={{
         accessToken: 'requests_are_mocked',
